95747550fb1aaf167529cb96759ce3d3521e1e34
m22 <mwendwamontel22@gmail.com>
2026-06-09 01:44:44 +0300

    fix: new changes 28(attendant, PM, sales,stores)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ import StoreLayout          from './pages/store/StoreLayout';
 // Attendant pages
 import { AttendantHome }    from './pages/attendant/AttendantHome';
 import { EggCollectionPage } from './pages/attendant/EggCollectionPage';
+import { BrooderPage } from './pages/attendant/BrooderPage';
 
 
 // Manager pages
